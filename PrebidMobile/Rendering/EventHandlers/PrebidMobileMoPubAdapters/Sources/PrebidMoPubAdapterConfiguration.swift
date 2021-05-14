cf38e95fbf0d714b3c8bbed351f07d909017f6cf
Yuriy Velichko <yuriy.velichko@openx.com>
2021-05-14 18:19:18 +0300

    refactor (Migration to Swift): migrate Configuration class and remove all other unneeded objc classes

---
+++ PrebidMobile/Rendering/EventHandlers/PrebidMobileMoPubAdapters/Sources/PrebidMoPubAdapterConfiguration.swift
@@ -0,0 +1,38 @@
+//
+//  PrebidMoPubAdapterConfiguration.swift
+//  PrebidMobileMoPubAdapters
+//
+//  Copyright © 2021 Prebid. All rights reserved.
+//
+
+import Foundation
+
+import MoPubSDK
+
+import PrebidMobileRendering
+
+@objc(PrebidMoPubAdapterConfiguration)
+public class PrebidMoPubAdapterConfiguration : MPBaseAdapterConfiguration {
+    
+    // MARK: - MPAdapterConfiguration
+    
+    public override var adapterVersion: String {
+        "\(PBMSDKConfiguration.sdkVersion).\(Constants.adapterVersion)"
+    }
+    
+    public override var networkSdkVersion: String {
+        PBMSDKConfiguration.sdkVersion
+    }
+    
+    public override func initializeNetwork(withConfiguration configuration: [String : Any]?, complete: ((Error?) -> Void)? = nil) {
+        PBMSDKConfiguration.initializeSDK()
+        
+        PBMSDKConfiguration.singleton.logLevel = .info
+        PBMSDKConfiguration.singleton.locationUpdatesEnabled = true
+        PBMSDKConfiguration.singleton.creativeFactoryTimeout = 15
+        
+        complete?(nil)
+    }
+ 
+    
+}

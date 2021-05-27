388c94408027720f412933b281058809b0db443c
VadimKhohlovOpenX <vadim.khohlov@openx.com>
2021-05-27 17:59:46 +0300

    refactor(SDK): fix tests after renaming

--- PrebidMobile/Rendering/PrebidMobileRendering/PrebidMobileRenderingTests/Prebid/PBMBidResponseTransformerTest.swift
+++ PrebidMobile/Rendering/PrebidMobileRendering/PrebidMobileRenderingTests/Prebid/PBMBidResponseTransformerTest.swift
@@ -119,7 +119,7 @@ class PBMBidResponseTransformerTest: XCTestCase {
         let serverResponse = PBMBidResponseTransformer.buildResponse(responseBody)
         let response = try! PBMBidResponseTransformer.transform(serverResponse)
         
-        func checkReplacements(keyPath: KeyPath<PBMBid, String?>, src: String) {
+        func checkReplacements(keyPath: KeyPath<Bid, String?>, src: String) {
             var expectedResult = src
             for (key, value) in replacements {
                 expectedResult = expectedResult.replacingOccurrences(of: "${\(key)}", with: value)
